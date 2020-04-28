29275525092daa740c5bb82d58fd1a85030b0c12
Felix Xu <gb0299@gmail.com>
2020-04-28 08:09:33 +0800

    Designed the tables of database and in db_processor.py, not completed.

---
+++ src/db_processor.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+def create_jones_cup_db():
+    print('Creating Database...')
+
+    connection = sqlite3.connect('jones_cup_db')
+    cursor = connection.cursor()
+
+    sql = """CREATE TABLE IF NOT EXISTS game (
+        id,
+        start_time,
+        win_team_id,
+        lose_team_id,
+        win_team_score,
+        lose_team_score,
+        season_id
+        )"""
+    cursor.execute(sql)
+
+    sql = """CREATE TABLE IF NOT EXISTS player (
+        id,
+        name,
+        height,
+        weight,
+        position,
+        two_point,
+        three_point,
+        rebound,
+        team_id
+        )"""
+    cursor.execute(sql)
+
+    sql = """CREATE TABLE IF NOT EXISTS season (
+        id,
+        year,
+        name,
+    )
+    """
+    cursor.execute(sql)
+
+    sql = """CREATE TABLE IF NOT EXISTS team (
+        id,
+        name
+    )
+    """
+    cursor.execute(sql)
+
+    connection.commit()
+    connection.close()
+
+if __name__ == '__main__':
