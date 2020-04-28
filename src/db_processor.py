3a90fdc27f3fcd6c1fc691102d59ba5e31bb442f
Felix Xu <gb0299@gmail.com>
2020-04-28 16:10:51 +0800

    1. Completed the jones cup database creating function in db_processor.py. 2. Add src/test.py to .gitignore.

--- src/db_processor.py
+++ src/db_processor.py
@@ -1,51 +1,71 @@
+import os
 import sqlite3
 
 def create_jones_cup_db():
     print('Creating Database...')
 
-    connection = sqlite3.connect('jones_cup_db')
+    if os.path.isdir(r'../db') != True:
+        os.mkdir(r'../db')
+
+    connection = sqlite3.connect(r'../db/jones_cup_db.sqlite')
     cursor = connection.cursor()
 
     sql = 'CREATE TABLE IF NOT EXISTS game (\
-        id INTEGER PRIMARY KEY IS NOT NULL,\
-        start_time,\
-        win_team_id,\
-        lose_team_id,\
-        win_team_score,\
-        lose_team_score,\
-        season_id\
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+        start_time TEXT,\
+        win_team_id INTEGER,\
+        lose_team_id INTEGER,\
+        win_team_score INTEGER,\
+        lose_team_score INTEGER,\
+        season_id INTEGER\
         )'
     cursor.execute(sql)
 
     sql = 'CREATE TABLE IF NOT EXISTS player (\
-        id,\
-        name,\
-        height,\
-        weight,\
-        position,\
-        two_point,\
-        three_point,\
-        rebound,\
-        team_id\
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+        name TEXT NOT NULL,\
+        age INTEGER,\
+        jersey INTEGER,\
+        height INTEGER,\
+        weight INGEGER,\
+        position TEXT\
+        )'
+    cursor.execute(sql)
+
+    sql = 'CREATE TABLE IF NOT EXISTS game_player_record (\
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+        game_id INTEGER NOT NULL,\
+        player_id INTEGER NOT NULL,\
+        play_time INTEGER,\
+        two_PT_all INTEGER,\
+        two_PT_success INTEGER,\
+        three_PT_all INTEGER,\
+        threethree_PT_success INTEGER,\
+        free_throw INTEGER,\
+        turnover INTEGER,\
+        foul INTEGER,\
+        steal INTEGER,\
+        rebound INTEGER,\
+        assist INTEGER,\
+        block INTEGER\
         )'
     cursor.execute(sql)
 
     sql = 'CREATE TABLE IF NOT EXISTS season (\
-        id,\
-        year,\
-        name,\
-    )
-    '
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+        year INTEGER NOT NULL,\
+        name TEXT NOT NULL\
+        )'
     cursor.execute(sql)
 
     sql = 'CREATE TABLE IF NOT EXISTS team (\
-        id,\
-        name\
-    )
-    '
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+        name TEXT NOT NULL\
+        )'
     cursor.execute(sql)
 
     connection.commit()
     connection.close()
 
 if __name__ == '__main__':
+    create_jones_cup_db()
