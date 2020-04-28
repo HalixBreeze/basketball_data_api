ac706fb06645abc0443a011a717d6849bb52747f
Felix Xu <gb0299@gmail.com>
2020-04-28 18:07:21 +0800

    Start to edit api.py.

--- src/api.py
+++ src/api.py
@@ -1,1 +1,14 @@
 from flask import Flask
+
+app = Flask('__name__')
+
+@app.route('/')
+def index():
+    return 'Home Page'
+
+@app.route('/teams')
+def get_teams():
+    return 'Teams'
+
+if __name__ == '__main__':
+    app.run()
