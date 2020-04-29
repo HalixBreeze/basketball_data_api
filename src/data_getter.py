b3ee82188ba455a8f8dbcc11380a0c691a6e1389
Felix Xu <gb0299@gmail.com>
2020-04-29 17:58:14 +0800

    Modified data_getter.py, not completed, v1.

--- src/data_getter.py
+++ src/data_getter.py
@@ -1,1 +1,17 @@
-import requests_html
+from  requests_html import HTMLSession
+import json
+
+def get_game(url):
+    pass
+
+if __name__ == '__main__':
+    session = HTMLSession()
+    url = 'https://choxue.com/api/v1/v7/legacy/v2/seasons/WJC40_FEMALE_jYGbCAfRc/teams'
+    response = session.get(url)
+    html = response.html
+
+    teams = json.loads(html.html)
+
+    for team in teams:
+        print(team.keys())
+        print(team.values())
